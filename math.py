43e6b5c46ad0e9b9fc53091e58cf737ffce68a9b
harshithashetty243-cmd <harshithashetty243@gmail.com>
2025-10-25 08:24:59 +0530

    Add basic math operations in math.py

---
+++ math.py
@@ -0,0 +1,7 @@
+try:
+  a=float(input("Enter first number:"))
+  b=float(input("enter second number:"))
+  print(f"Addition:{a+b}")
+  print(f"Subtraction:{a-b}")
+except ValueError:
+  print("please enter valid number.")
